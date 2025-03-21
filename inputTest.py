1fd4c1be555467ad9be8b3fa7f5df6f5361d2a7c
leegaeun <yse060524>
2025-03-21 09:17:15 +0900

    2주차 실습 과제

--- inputTest.py
+++ inputTest.py
@@ -8,3 +8,18 @@ print(num2)
 
 numSum = num1 + num2
 print(numSum)
+
+numSub = num1 - num2
+print(numSub)
+
+numMul = num1 * num2
+print(numMul)
+
+numDiv = num1 / num2
+print(numDiv)
+
+numMod = num1 % num2
+print(numMod)
+
+numPower = num1 ** num2
+print(numPower)
