7b5f6eeb4f2d9f867b6097c5ffe7fcaab820e3b7
leegaeun <yse060524@gmail.com>
2025-04-07 10:53:54 +0900

    4주차

---
+++ 4week/제비뽑기.py
@@ -0,0 +1,36 @@
+import tkinter
+import random
+
+root = tkinter.Tk()
+root.title("캔버스 만들기")
+
+# 캔버스 생성
+canvas = tkinter.Canvas(root, width=800, height=600)
+canvas.pack()
+
+# 좌표 출력기
+def mouseMove(event):
+    x = event.x
+    y = event.y
+    labelMouse["text"] = str(x) + "," + str(y)
+
+root.bind("<Motion>", mouseMove) 
+
+# 랜덤 출력
+def click_btn():
+    label["text"] = random.choice(["대길","중길","소길","흉"])
+
+labelMouse = tkinter.Label(root, text=",", font=("맑은 고딕", 10))  
+labelMouse.pack()
+
+# 캔버스 이미지 생성
+bgimg = tkinter.PhotoImage(file="C:/Users/user/Desktop/python/4week/miko.png")  
+canvas.create_image(400, 300, image=bgimg)  
+
+label = tkinter.Label(root, text="??", font=("맑은 고딕", 120))
+label.place(x=380, y=60)
+
+button = tkinter.Button(root, text= "제비 뽑기", font= ("맑은 고딕", 36), command= click_btn )
+button.place(x=360, y= 400)
+
+root.mainloop()
