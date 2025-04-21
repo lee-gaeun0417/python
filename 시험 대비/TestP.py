c9d2780762febcce09ac9f50f13c3251b4cb3473
leegaeun <yse060524@gmail.com>
2025-04-21 10:55:25 +0900

    시험준비

---
+++ 시험 대비/TestP.py
@@ -0,0 +1,16 @@
+#실습 3번 택배
+# print(" ## 택배를 보내기 위한 정보를 입력하시오. ##")
+# name = input("받는 사람: ")
+# ad = input("주소:  ")
+# wh = input("무게(g): ")
+# print("받는 사람 :", name)
+# print(" 주소 : ", ad)
+# print("배송비 : ", int(wh)*5)
+
+#문자열 알아보기 실습 트와이스
+strin = input("원본 문자열 ==> ")
+strout = strin[3]+strin[2]+strin[1]+strin[0]
+
+n = len(strin)
+print(n)
+print("반대 문자열 ==>", strout)
