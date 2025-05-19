bfe64e786e4571016aec1d1801d8e30eb481f544
leegaeun <yse060524@gmail.com>
2025-05-19 11:53:25 +0900

    10주차

--- 4week/제비뽑기.py
+++ 4week/제비뽑기.py
@@ -19,6 +19,7 @@ root.bind("<Motion>", mouseMove)
 # 랜덤 출력
 def click_btn():
     label["text"] = random.choice(["대길","중길","소길","흉"])
+    text.insert(tkinter.END,label["text"]+"\n")
 
 labelMouse = tkinter.Label(root, text=",", font=("맑은 고딕", 10))  
 labelMouse.pack()
@@ -27,10 +28,16 @@ labelMouse.pack()
 bgimg = tkinter.PhotoImage(file="C:/Users/user/Desktop/python/4week/miko.png")  
 canvas.create_image(400, 300, image=bgimg)  
 
+canvas.create_rectangle(350,390,620,520, fill='darkgray', outline ='red', width='3')
+
 label = tkinter.Label(root, text="??", font=("맑은 고딕", 120))
 label.place(x=380, y=60)
 
 button = tkinter.Button(root, text= "제비 뽑기", font= ("맑은 고딕", 36), command= click_btn )
 button.place(x=360, y= 400)
+
+text = tkinter.Text()
+text.pack()
+
 
 root.mainloop()
